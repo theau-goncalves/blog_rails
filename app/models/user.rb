643bc49cd7c24a3a7c3b6ac115ed3f310b1415fe
theau-goncalves <theau26@gmail.com>
2017-01-15 00:55:01 +0100

    Gestion des utilisateurs (gem devise) et ajout  connexion par email ou login

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
+
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where("lower(username) = :value OR lower(email) = :value", value: login.downcase).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  
+  devise_for :users
+  get 'home/private'
 
   root to: 'pages#home'
   get '/salut(/:name)', to: 'pages#salut', as: 'salut'
   resources :posts
+  
 
 
   # For details ,on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
