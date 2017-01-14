753a44f1adf7ba6c021856d454ce184ca76247c7
theau-goncalves <theau26@gmail.com>
2017-01-14 18:03:10 +0100

    Création du model post et mise en place de la page index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  
+
   root to: 'pages#home'
   get '/salut(/:name)', to: 'pages#salut', as: 'salut'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/articles', to: 'posts#index', as: 'posts'
+  # For details ,on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
