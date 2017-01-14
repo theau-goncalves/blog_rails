cda6dfaefc0f797c2eb562dacdff9a4006a5718f
theau-goncalves <theau26@gmail.com>
2017-01-14 20:11:30 +0100

    Implementation du CRUD pour les post

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get '/salut(/:name)', to: 'pages#salut', as: 'salut'
-  get '/articles', to: 'posts#index', as: 'posts'
+  resources :posts
+
+
   # For details ,on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
