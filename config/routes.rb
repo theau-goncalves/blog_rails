c427fd1a7b4ef54d9928e56b26a2ea2576c95e88
theau-goncalves <theau26@gmail.com>
2017-01-15 22:03:52 +0100

    Correction minime des controllers et mise en page edit post

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'home/private'
-
+  
   root to: 'posts#index'
-  get '/salut(/:name)', to: 'pages#salut', as: 'salut'
   resources :posts do
     resources :comments
   end
